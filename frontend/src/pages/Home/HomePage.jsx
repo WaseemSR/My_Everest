3e8801bf700b420d6af90cf78e56de3bad8dd7d4
WaseemSR <waseemrehman88@gmail.com>
2025-08-27 16:12:49 +0100

    create Navbar, Header and Footer components and import into HomePage and Login Page - unstyled - includes NavBar Tests

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -2,12 +2,19 @@ import { Link } from "react-router-dom";
 
 import "./HomePage.css";
 
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 export function HomePage() {
   return (
-    <div className="home">
-      <h1>Welcome to Acebook!</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
+    <div>
+      <Header showNav={false}/>
+      <main className="home">
+        <h1>Welcome to Acebook!</h1>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Log In</Link>
+      </main>
+      <Footer/>
     </div>
   );
 }
