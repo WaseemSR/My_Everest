e0eae2b6fa17a3af11924332b56594a615347ffa
Louis Johnson <louisj157@gmail.com>
2025-08-28 10:09:56 +0100

    html tag fixes in HomePage.jsx

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -7,22 +7,23 @@ import Footer from "../../components/Footer";
 export function HomePage() {
   return (
     
-   <div>
-   <Header showNav={false}/>
-    <main className="home">
-      <div className="home-content">
-        <h1 className="home-title">Welcome to MyEverest!</h1>
-        <div className="home-buttons">
-          <Link to="/signup" className="home-button">
-            Sign Up
-          </Link>
-          <Link to="/login" className="home-button">
-            Log In
-          </Link>
+  <div>
+    <Header showNav={false}/>
+      <main className="home">
+        <div className="home-content">
+          <h1 className="home-title">Welcome to MyEverest!</h1>
+          <div className="home-buttons">
+            <Link to="/signup" className="home-button">
+              Sign Up
+            </Link>
+            <Link to="/login" className="home-button">
+              Log In
+            </Link>
+          </div>
         </div>
       </main>
-    </div>
-   </div>
+      <Footer/>
+  </div>
   );
 }
 
