cdb5ea58f2383a2c9f6cfd154e90bd0c4309cb85
christopher <chris.holfve@hotmail.com>
2025-08-29 14:43:22 +0100

    seperated CI for backend and frontend instead of one

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+    coverage: {
+      provider: "v8", // 👈 use @vitest/coverage-v8
+      reporter: ["text", "html"], // text summary + HTML report
+      reportsDirectory: "./coverage", // output folder (default is 'coverage')
+    },
   },
 });
