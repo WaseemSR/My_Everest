f21ec41a9196ec157b5f5b44349a7af41b0cea7f
chris <chris.holfve@hotmail.com>
2025-09-24 10:50:50 +0100

    created small catch phrase

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -2,14 +2,14 @@
 // const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;  // connect to back end if needed
 
 
-function User ({ user}) {
+function User ({ user }) {
     if (!user) return null;
 
     return (
         <article>
             <div >
 
-                <h2>{user.fullName}</h2>
+                <h2>{user.username}</h2>
 
                 {/* <p>{user.bio}</p> */}
             </div>
