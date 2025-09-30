81f145754b0b9a23782e2422420c58b9edb6f511
chris <chris.holfve@hotmail.com>
2025-09-30 16:00:55 +0100

    added quote to explore everests page and updated about

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -49,9 +49,13 @@ export function FeedPage() {
 
       <Header showNav={true} />
 
-      
         <main className="is-flex-grow-1 p-5" style={{ backgroundColor: "#1b262c" }}>
-          <h1 className="has-text-weight-light mt-6 mb-6 has-text-white my-mega-title">Explore Everests</h1> <br />    {/*  This is the small bit added for the everests to be seen on feedpage */}
+          <h1 className="has-text-weight-light mt-6 mb-6 has-text-white my-mega-title">Explore Everests</h1> <br />
+          <p className="box is-size-4 has-text-white has-text-weight-light has-text-centered" style={{ maxWidth: "70rem", background: "transparent", margin: "0 auto" }} >
+            Every Everest tells a story of ambition, persistence, and progress. Here, you can see the peaks others are chasing, from 
+            bold life goals to everyday triumphs and find inspiration for your own journey. Because when you witness what’s possible, 
+            your next Everest starts to feel within reach.</p>
+
           <div className="columns is-multiline equal-columns mt-6">
             {everests.map((ev) => (
               <EverestCard key={ev._id} everest={ev} />
